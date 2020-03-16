38d7bbe78dc67ab60907fda96680749946f321b2
Eskil Oscar Ehrensverd <eskil.o.svendsen@gmail.com>
2020-03-16 21:32:02 +0100

    Upated DatabaseHandler with new database user made basic decorator for querying the database

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -3,30 +3,46 @@ import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
-password = os.getenv('DB_PW')
+env_pw = os.getenv('DB_PW')
 
-try:
-    connection = psycopg2.connect(user='eskil',
-                                  password=password,
-                                  host='localhost',
-                                  port='5432',
-                                  database='test')
 
-    cursor = connection.cursor()
-    # Print propertiies of connection
-    print(connection.get_dsn_parameters(), '\n')
+
+
+def db_connector(func):
+    def wrapper_connection_(*args, **kwargs):
+        try:
+            print('ihappen')
+            connection = psycopg2.connect(user='testbot',
+                                          password=env_pw,
+                                          host='localhost',
+                                          port='5432',
+                                          database='test')
+            cursor = connection.cursor()
+            # Print propertiies of connection
+            print(connection.get_dsn_parameters(), '\n')
     
-    # Print PSQL version
-    cursor.execute('SELECT version();')
-    record = cursor.fetchone()
-    print('You are connected to: ', record, '\n')
+            # Print PSQL version
 
-except (Exception, psycopg2.Error) as error :
-    print('Error connecting to PSQL: ', error)
+            query_function = func(cursor, *args, **kwargs)
+            record = cursor.fetchone()
+            print('You are connected to: ', record, '\n')
 
-finally:
-    # close db connection
-    if (connection):
-        cursor.close()
-        connection.close()
-        print('PSQL connection closed')
+
+        except (Exception, psycopg2.Error) as error :
+            print('Error connecting to PSQL: ', error)
+
+        finally:
+            # close db connection
+            if (connection):
+                cursor.close()
+                connection.close()
+                print('PSQL connection closed')
+        return query_function
+    return wrapper_connection_
+
+@db_connector
+def test(cursor):
+    return cursor.execute('SELECT version();')
+
+
+test()
