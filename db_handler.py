2b254310cdd4269714810b6a65db080d01d805b4
h587731 <mail.eskil@gmail.com>
2020-03-26 16:37:03 +0100

    Removed redundant comments and print statements in db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -16,9 +16,6 @@ def db_connector(func):
                                           database='test')
 
             cursor = connection.cursor()
-            # Print propertiies of connection
-            #print('\nDB Connection opend')
-    
             # Query Function to be called
             query_function = func(cursor, *args, **kwargs)
             connection.commit()
@@ -30,16 +27,8 @@ def db_connector(func):
             if (connection):
                 cursor.close()
                 connection.close()
-                #print('DB connection closed\n')
         return query_function
     return wrapper_connection_
-
-@db_connector
-def test(cursor):
-    print('Start of test')
-    cursor.execute('SELECT version();')
-    record = cursor.fetchone()
-    print('You are connected to: ', record, '\n')
 
 
 @db_connector
@@ -48,32 +37,26 @@ def insert_user(cursor, user_tuple):
     print('Inserting : ', user_tuple, 'into db')
     postgres_insert_query = """ INSERT INTO discord_users (user_id, username, discriminator, user_nick) VALUES (%s,%s,%s,%s) ON CONFLICT DO NOTHING"""
     cursor.execute(postgres_insert_query, user_tuple)
-    print('Setting up score for ', user_tuple[1])
     postgres_insert_query = """ INSERT INTO score (user_id) VALUES ((SELECT user_id from discord_users WHERE user_id=%s)) ON CONFLICT DO NOTHING"""
     cursor.execute(postgres_insert_query, (user_tuple[0],))
-    print('Added user: ', user_tuple, 'to database and')
 
 
 @db_connector
 def update_member(cursor, member_tuple):
     """ Updates name, nick or discriminator of existing member by id"""
     cursor.execute('SELECT * FROM discord_users where user_id=%s', (member_tuple[0],))
-    print('updating user:' , cursor.fetchone())
-
-    record_to_update = (member_tuple[1], member_tuple[2], member_tuple[3], member_tuple[0])
+    parameters = (member_tuple[1], member_tuple[2], member_tuple[3], member_tuple[0])
     postgres_update_query = """ UPDATE discord_users set username = %s, discriminator = %s, user_nick = %s where user_id=%s"""
-    cursor.execute(postgres_update_query, record_to_update)
-
+    cursor.execute(postgres_update_query, parameters)
     cursor.execute(""" SELECT * FROM discord_users where user_id=%s""", (member_tuple[0],))
-    print('user updated to:', cursor.fetchone())
 
 
 @db_connector
 def select_all_members(cursor):
     """Returns all members of db table discord_users"""
-    print('Retrieving all discord users')
     cursor.execute('SELECT * FROM discord_users ;')
     return cursor.fetchall()
+
 
 @db_connector
 def find_member_id(cursor, user_id):
@@ -86,57 +69,45 @@ def find_member_id(cursor, user_id):
 
 @db_connector
 def insert_ping_event(cursor, timestamp):
-    """Inserts an  ping event."""
-    #Getting starttime
-
-    print('Setting previous ping_event active to false')
-    cursor.execute( """ UPDATE ping_events set active=FALSE where active=TRUE""")
-
-    print('Setting ping to be scored to active to true')
+    """Inserts an  ping event and updates all necessary fields
+    Sets all ping events active field to false
+    Sets new ongoing ping event active to true
+    Inserts tomorrows ping even with active to false
+    Updates score table collum has scored to False and Daily score to 0
+    """
+    cursor.execute(""" UPDATE ping_events set active=FALSE where active=TRUE""")
     cursor.execute(""" UPDATE ping_events set active=TRUE where ping_id IN( SELECT max(ping_id) FROM ping_events);""")
-
-    print('Inserting next ping event with timestamp: ', timestamp)
     postgres_insert_query = """ INSERT INTO ping_events (ping_time, active) VALUES (%s,FALSE) ON CONFLICT DO NOTHING"""
-
-    print('Setting has scored to false for all users:')
     cursor.execute(""" UPDATE score set has_scored=default, daily_score=default;""")
-
     cursor.execute(postgres_insert_query, (timestamp,))
-    print('Ping event inserted.')
+
 
 
 @db_connector
 def query_timestamp_next_ping(cursor):
-    print('Next ping queried')
+    #print('Next ping queried')
     cursor.execute("""SELECT ping_time FROM ping_events WHERE ping_id IN(SELECT max(ping_id) FROM ping_events)""")
-    record = cursor.fetchone()
-    print('from db ', record)
-    return record
+    return cursor.fetchone()
+
 
 @db_connector
 def query_timestamp_ongoing_ping(cursor):
-    print('Ongoing ping queried')
+    #print('Ongoing ping queried')
     cursor.execute("""SELECT ping_time FROM ping_events WHERE active=TRUE""")
-    record = cursor.fetchone()
-    print('from db ', record)
-    return record
+    return cursor.fetchone()
 
 
 @db_connector
 def query_has_scored(cursor, user):
-    print('Query if member har scored', user)
+    #print('Query if member har scored', user)
     postgres_select_query = """SELECT has_scored FROM score WHERE score.user_id=%s """
-
     cursor.execute(postgres_select_query, (user,))
-
-    record = cursor.fetchone()
-    print('from db ', record)
-    return record[0]
+    return cursor.fetchone()[0]
 
 
 @db_connector
 def insert_score(cursor, delta, user):
-    print("Inserting score", delta, " to users id ", user)
+    #print("Inserting score", delta, " to users id ", user)
     query_string = """ UPDATE score set has_scored=True, daily_score=(%s), total=total+(%s) where score.user_id=%s """
     cursor.execute(query_string, (int(delta), int(delta), user))
     cursor.connection.commit()
